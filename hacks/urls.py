46f3174c14cc946eb07835822857d2d000936b56
Ben Webb <bjwebb67@googlemail.com>
2013-04-03 20:04:56 +0100

    Make whorunsthatbus it's own project

--- hacks/urls.py
+++ hacks/urls.py
@@ -7,14 +7,9 @@ from django.conf.urls.static import static
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'hacks.views.home', name='home'),
-    # url(r'^hacks/', include('hacks.foo.urls')),
+    url(r'^whorunsthatbus/', include('whorunsthatbus.urls')),
+    url(r'^mbtilesmap/', include('mbtilesmap.urls', namespace='mb', app_name='mbtilesmap')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('mbtilesmap.urls', namespace='mb', app_name='mbtilesmap')),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

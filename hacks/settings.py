fc4978a115b5c28a7785854c4b27e1b4c5721e9c
Ben Webb <bjwebb67@googlemail.com>
2014-12-21 11:11:02 +0000

    Update requirements.txt

--- hacks/settings.py
+++ hacks/settings.py
@@ -1,6 +1,6 @@
 # Django settings for hacks project.
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -46,7 +46,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = '/home/bjwebb/django/hacks/media/'
+MEDIA_ROOT = 'media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
